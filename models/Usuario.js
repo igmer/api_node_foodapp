caca9fbb4762ca09a09113d0077f31741126a90a
igmer <igmerdz@gmail.com>
2019-10-28 22:53:55 -0600

    login

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,12 +1,12 @@
 let Sequelize = require('sequelize');
 const db = require('../config/database');
 const Usuario = db.define('usuarios', {
-    idUsuario: { type: Sequelize.INTEGER, primaryKey: true,autoIncrement: true },
+    id: { type: Sequelize.INTEGER, primaryKey: true,autoIncrement: true },
     Nombre:Sequelize.STRING,
     usuario:Sequelize.STRING,
     password:Sequelize.STRING,
     activo:Sequelize.STRING,
-    sucursal:Sequelize.STRING,
+    Idsucursal:Sequelize.STRING,
     deleted:Sequelize.STRING,
     createdAt:Sequelize.STRING,
     updatedAt:Sequelize.STRING
